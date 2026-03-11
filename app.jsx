5719087c6f88ffa4b86d3c370923498198a02a19
PRANESH-KV <praneshkv2@gmail.com>
2026-03-12 01:34:46 +0530

    app.jsx

---
+++ app.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NotificationSystem from "./NotificationSystem";
+
+function App() {
+  return (
+    <div>
+      <h1>Frontend User Notification System</h1>
+      <NotificationSystem />
+    </div>
+  );
+}
+
+export default App;
